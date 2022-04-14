6c774db50555f7280dd4ffdc792a2bf03a3f2408
Roman Ojha <razzroman98@gmail.com>
2022-04-14 19:46:40 +0545

    Structure of testing on react-testing-library

---
+++ Tutorial/01_react_testing_library/src/Class/04_Structure_Of_Test/index.js
@@ -0,0 +1,16 @@
+/*
+    -> When you would install react app you would get these testing library install automatically:
+        -> "@testing-library/jest-dom": "^5.11.4",
+            "@testing-library/react": "^11.1.0",
+            "@testing-library/user-event": "^12.1.10",
+
+    -> Inside package.json we have test script:
+        -> "test": "react-scripts test",
+        -> So, run:
+            -> npm run test
+        -> and this will run 'App.test.js' file
+        -> Note what ever test file name you will write it will test that component
+            -> EX: test file name 'Roman.test.js'
+            -> now this file will test 'Roman' Component
+    -> now let us go to 'App.test.js' and see what is happening there
+*/
