054690fc3573b6e494377d9742bcdbcbde04988c
Roman Ojha <razzroman98@gmail.com>
2022-04-14 19:11:55 +0545

    initilize react-testing-library tut

---
+++ Tutorial/01_react_testing_library/src/Class/02_The_Importance_of_Testing/index.js
@@ -0,0 +1,15 @@
+/*
+    *) React Testing Library:
+        -> This will allow us to test our react application
+    => For this project we have created the 'Todo' application where we except to have certain functionality up on running
+        -> for EX: when we type into input field we want the value to be change
+            -> and when we click the 'Add' button we would except input field value to disappear and add into todo list
+            -> ETC..
+        -> here we are testing these thing manually but react-testing-library will allow us to automate these testing
+        -> so now all of these test would be written in code
+    *) Important of Testing:
+        1) Catch Bugs
+        2) Increase confidence in application
+        3) Speeds up QA time
+        4) Can Server as Documentations
+*/
