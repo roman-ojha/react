16eb31b5ed617436d65507122389a73e62255398
Roman Ojha <razzroman98@gmail.com>
2022-04-14 19:21:45 +0545

    type of test in react testing

---
+++ Tutorial/01_react_testing_library/src/Class/03_Tyes_of_Test/index.js
@@ -0,0 +1,12 @@
+/*
+    *) Type of Test:
+        -> There is many different type of test but we would go into some of important one
+            1) Unit Tests
+                -> Unit Test is the test that test a piece of code a component in complete isolation
+                -> It means that we will just test one single component
+            2) Integration Tests
+                -> Integration test is the test which will test overall component 
+                -> EX: test about how these component are working with each other
+            3) End to End (E2E) Tests
+                -> https://youtu.be/n_sS-GAgZ98?list=PL4cUxeGkcC9gm4_-5UsNmLqMosM-dzuvQ&t=198
+*/
